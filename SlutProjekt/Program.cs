57a7a014ff3e34694328cfc30544b8132f00cebb
Elliot <elliot.widell@elev.ga.ntig.se>
2021-03-29 09:36:21 +0200

    2021-03-29 commit

--- SlutProjekt/Program.cs
+++ SlutProjekt/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Raylib_cs;
 
 namespace SlutProjekt
 {
@@ -6,7 +8,32 @@ namespace SlutProjekt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Raylib.InitWindow(1200, 800, "Zombie survival");
+            Raylib.BeginDrawing();
+            map();
+            player();
+
+
+            Console.ReadLine();
+            Raylib.EndDrawing();
+
+
+        }
+        static void player()
+        {
+            int playerX = 600;
+            int playerY = 400;
+            Raylib.DrawCircle(playerX, playerY, 30, Color.BLUE);
+
+
+
+        }
+
+        static void map()
+        {
+            Raylib.DrawRectangle(0, 0, 1200, 800, Color.GREEN);
+
+
         }
     }
 }
